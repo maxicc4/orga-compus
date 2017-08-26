88b3bff3ecfe770fd73408dea2bed08f1348e47b
DMaitia <dariusmaitia@gmail.com>
2017-08-26 17:35:54 -0300

    Terminado. Menu completo y manejo de argumentos de entrada hecho. Falta testearlo a lo loco, pero por ahora no me ha fallado.

--- main.c
+++ main.c
@@ -46,15 +46,71 @@ int processFile(file_t * input_file, file_t * output_file){
       }
     }
   }
+  return SUCCESS;
+}
+
+void show_help_menu(){
+  printf("%s","Usage:\n\t"
+      "tp0 -h\n\t"
+      "tp0 -V\n\t"
+      "tp0 [options]\n"
+      "Options:\n\t"
+      "-V, --version \tPrint version and quit.\n\t"
+      "-h, --help \tPrint this information.\n\t"
+      "-i, --input \tLocation of the input file.\n\t"
+      "-o, --output \tLocation of the output file.\n"
+      "Examples:\n\t"
+      "tp0 -i ~/input -o ~/output\n");
 }
 
 int main(int argc, char *argv[]) {
   file_t input_file;
   file_t output_file;
-  openFile(&input_file, "../archivo.txt", "r");
-  openFile(&output_file, "../salida.txt", "w");
-  processFile(&input_file, &output_file);
+  bool input_path_found;
+  bool output_path_found;
+  int i,s;
+
+  if (argc == 2 && (!strcmp(argv[1],"-h") || !strcmp(argv[1],"--help"))){
+    show_help_menu();
+    return SUCCESS;
+  }
+  if (argc == 2 && (!strcmp(argv[1],"-v") || !strcmp(argv[1],"--version"))){
+    printf("%s","Palindrom Finder: v1.0\n");
+    return SUCCESS;
+  }
+
+  input_path_found = false;
+  i = 1;
+  while (!input_path_found && i < argc){
+    if (!strcmp(argv[i],"-i")){
+      input_path_found = true;
+    } else {
+      i++;
+    }
+  }
+  if (input_path_found){
+    openFile(&input_file, argv[i+1], "r");
+  } else {
+    input_file.file = stdin;
+  }
+
+  output_path_found = false;
+  i = 1;
+  while (!output_path_found && i < argc){
+    if (!strcmp(argv[i],"-o")){
+      output_path_found= true;
+    } else {
+      i++;
+    }
+  }
+  if (output_path_found){
+    openFile(&output_file, argv[i+1], "w");
+  } else {
+    output_file.file = stdout;
+  }
+
+  s = processFile(&input_file, &output_file);
   closeFile(&input_file);
   closeFile(&output_file);
-  return 0;
+  return s;
 }
