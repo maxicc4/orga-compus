f0ca3b6e3c6af1f2f2684dee1a9fc285f2042448
DMaitia <dariusmaitia@gmail.com>
2017-09-21 14:19:50 -0300

    Intentando arreglar la cagada que me mande

---
+++ cmake-build-debug/CMakeFiles/TP1_Orga.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1_Orga.dir/main.c.o"
+  "TP1_Orga.pdb"
+  "TP1_Orga"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP1_Orga.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
